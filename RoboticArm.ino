883f7c3d01a4d1d08f304f649d924f708bbbdd6b
Kyle Dorny <kyledorny@gmail.com>
2017-09-01 10:36:59 -0700

    Found old file

--- RoboticArm.ino
+++ RoboticArm.ino
@@ -13,11 +13,49 @@ void setup() {
   stepper1.setStepsPerRevolution(200 * microstepping * transmission);
   stepper1.setSpeedInRevolutionsPerSecond(stepperSpeed);
   stepper1.setAccelerationInRevolutionsPerSecondPerSecond(acceleration);
-
+ 
+ homing();
 }
 
 void loop() {
 
-  stepper1.moveToPositionInRevolutions(-1.5);
-  stepper1.moveToPositionInRevolutions(1.5);
+if(!digitalRead(A2)){
+  moveMagnetTallToShort(1000);
 }
+
+delay(2000);
+
+if(!digitalRead(A3)){
+  moveMagnetShortToTall(1000);
+}
+
+delay(2000);
+
+}
+
+void homing() {
+   const int directionTowardHome = 1;
+  const float speedInRevolutionsPerSecond = .25;
+  const float maxDistanceToMoveInRevolutions = 1.5;
+  const byte homeSwitchPin = A1;
+
+  stepper1.moveToHomeInRevolutions(directionTowardHome, speedInRevolutionsPerSecond, maxDistanceToMoveInRevolutions, homeSwitchPin);
+}
+
+void moveMagnetShortToTall(int pickupTime) {
+  stepper1.moveToPositionInRevolutions(-.575);
+  delay(pickupTime);
+  stepper1.moveToPositionInRevolutions(-.75);
+  delay(pickupTime);
+  stepper1.moveToPositionInRevolutions(0);
+
+}
+ void moveMagnetTallToShort(int pickupTime) {
+   stepper1.moveToPositionInRevolutions(-.75);
+  delay(pickupTime);
+  stepper1.moveToPositionInRevolutions(-.575);
+  delay(pickupTime);
+  stepper1.moveToPositionInRevolutions(0);
+
+ }
+
